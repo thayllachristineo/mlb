aca5ac4d39c5c85a46d620ec9e3f5af587a29304
thaylla <thayllachristineo@gmail.com>
2024-07-03 18:39:04 -0300

    feat: configure App routes

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,27 @@
+import { FC } from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+import Search from '@components/Search';
+import { AppProvider } from '@contexts/AppContext';
+import ItemDetails from '@pages/ItemDetails';
+import ItemsResult from '@pages/ItemResult';
 import GlobalStyle from '@styles/GlobalStyle';
 
-function App() {
+const App: FC = (): JSX.Element => {
   return (
-    <>
-      <GlobalStyle />
-    </>
+    <AppProvider>
+      <Router>
+        <GlobalStyle />
+        <Search />
+
+        <Routes>
+          <Route path="/items" element={<ItemsResult />} />
+          <Route path="/items/:id" element={<ItemDetails />} />
+          <Route path="/" />
+        </Routes>
+      </Router>
+    </AppProvider>
   );
-}
+};
 
 export default App;
